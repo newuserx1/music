ae3bbc5a66d1355c97712c8c33840c19dbd2b967
newuserx1 <121651688+newuserx1@users.noreply.github.com>
2023-03-14 13:52:40 +0530

    Update queue.py

--- YukkiMusic/utils/stream/queue.py
+++ YukkiMusic/utils/stream/queue.py
@@ -41,6 +41,7 @@ async def put_queue(
         "vidid": vidid,
         "seconds": duration_in_seconds,
         "played": 0,
+        "Powered By @Music_Forever_X",
     }
     if forceplay:
         check = db.get(chat_id)
@@ -84,6 +85,7 @@ async def put_queue_index(
         "vidid": vidid,
         "seconds": 0,
         "played": 0,
+        "Powered By @Music_Forever_X",
     }
     if forceplay:
         check = db.get(chat_id)
